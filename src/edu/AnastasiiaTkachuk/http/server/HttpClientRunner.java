7ed4fcb3641e65e2b59809f1e34285793e689456
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-02-04 14:54:48 +0200

    add pool of client and sendAsync request

--- src/edu/AnastasiiaTkachuk/http/server/HttpClientRunner.java
+++ src/edu/AnastasiiaTkachuk/http/server/HttpClientRunner.java
@@ -7,9 +7,11 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Path;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class HttpClientRunner {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
         HttpClient httpClient = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_1_1)
                 .build();
@@ -18,8 +20,11 @@ public class HttpClientRunner {
                 .header("content-type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofFile(Path.of("resources", "first.json")))
                 .build();
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.headers());
-        System.out.println(response.body());
+        CompletableFuture<HttpResponse<String>> response = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        CompletableFuture<HttpResponse<String>> response2 = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        CompletableFuture<HttpResponse<String>> response3 = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+//        System.out.println(response.headers());
+//        System.out.println(response.body());
+        System.out.println(response3.get().body());
     }
 }
--- src/edu/AnastasiiaTkachuk/http/server/HttpServerRunner.java
+++ src/edu/AnastasiiaTkachuk/http/server/HttpServerRunner.java
@@ -2,7 +2,7 @@ package edu.AnastasiiaTkachuk.http.server;
 
 public class HttpServerRunner {
     public static void main(String[] args) {
-        HttpServer httpServer = new HttpServer(8088);
+        HttpServer httpServer = new HttpServer(8088, 100);
         httpServer.run();
     }
 }
